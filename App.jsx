595d5b22b8575cede29f571d8c9b69de11735f9f
jvillos <jvillosl14@gmail.com>
2025-01-08 14:15:16 +0100

    logo added

--- App.jsx
+++ App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
+import jsImage from "./assets/images/js-logo-xs.png"; //we import images this way and use it in the src above between {}
 
 export default function App() {
   //THIS IS JSX. A markup system simmilar to html that we use in react
+  //if you return more than one tag, you need to wrap it in one, such as an empty <></>
   return (
     <>
       <header>
-        <img src="assets/images/js-logo-xs.png" alt="JavaScript logo" />
+        <img src={jsImage} alt="JavaScript logo" />
         <h1>JavaScript Refresher</h1>
       </header>
 
